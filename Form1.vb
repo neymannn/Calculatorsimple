3fd169b8254e88cdebaaa9fc34fa30629e9f102a
user <user@PENDEK->
2022-03-23 23:13:28 +0800

    Add project files.

---
+++ Form1.vb
@@ -0,0 +1,27 @@
+﻿Public Class Form1
+    Private Sub btnSubmit_Click(sender As Object, e As EventArgs) Handles btnSubmit.Click
+        Dim str1 As Double = txt1.Text
+        Dim str2 As Double = txt2.Text
+
+        Dim str_add As String = str1 + str2
+        Dim str_minus As String = str1 - str2
+        Dim str_multiply As String = str1 * str2
+        Dim str_divide As String = str1 / str2
+        Dim str_modulus As String = str1 Mod str2
+
+        Label3.Text = "result : " + Environment.NewLine _
+        + "Sum : " + str_add + Environment.NewLine _
+        + "Different : " + str_minus + Environment.NewLine _
+        + "Product : " + str_multiply + Environment.NewLine _
+        + "Integer Quotient : " + str_modulus + Environment.NewLine _
+        + "Division: " + str_divide
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub txt2_TextChanged(sender As Object, e As EventArgs) Handles txt2.TextChanged
+
+    End Sub
+End Class
